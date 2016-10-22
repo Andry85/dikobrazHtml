601aabee6febefbd1874063b4c192682f607054a
andryShapoval <andryShapoval@gmail.com>
2016-10-22 15:03:56 +0300

    нові сторінки

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,24 @@ $(function() {
 	/*********************************************************/
 						/* End hack for IE8  */
 	/********************************************************/
+
+
+	/*********************************************************/
+						/* Start Tabs  */
+	/********************************************************/
+	$('#list li a').on('click', function(e){
+		e.preventDefault();
+		var $tabs = $("#tabs");
+		var $tabsList = $tabs.children();
+		$(this).parent('li').addClass('active').siblings().removeClass('active');
+		var $parent = $(this).parent('li');
+		var $parentIndex = $parent.index();
+		var $current = $tabsList[$parentIndex];
+		$($current).addClass('active').siblings().removeClass('active');
+	});
+	/*********************************************************/
+						/* End Tabs  */
+	/********************************************************/
 	 
 
 });
---
+++ src/js/map-contacts.js
@@ -0,0 +1,21 @@
+﻿ymaps.ready(init);
+        var myMap, myPlacemark;
+
+
+        function init(){ 
+            myMap = new ymaps.Map("map", {
+                center: [55.773945, 37.59109],
+                zoom: 16,
+				behaviors:['default', 'scrollZoom']
+            }); 
+            
+            myPlacemark = new ymaps.Placemark([55.773945, 37.59109], {
+                hintContent: 'г. Москва',
+                balloonContent: 'ул. 2-ая Тверская-ямская 40'
+            });
+            
+            myMap.geoObjects.add(myPlacemark);
+			myMap.behaviors.disable('drag');
+			myMap.options.set('scrollZoomSpeed', 0.5);
+
+        }
